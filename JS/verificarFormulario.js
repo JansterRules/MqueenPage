c2b25746e7a3951ae9095ebf79d30837d9fdee0c
bNegro <72948318+BNegro1@users.noreply.github.com>
2024-05-13 14:34:02 -0400

    Eliminación e integración de funciones en JS

---
+++ JS/verificarFormulario.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('#loginForm').submit(function(event) {
+        
+        event.preventDefault(); // Evitar el envío del formulario por defecto
+
+
+        $('.error-message').text(''); // Limpiar mensajes de errores
+
+        
+        var email = $('#exampleInputEmail1').val(); // Recuperamos email del campo de "email".
+        var password = $('#exampleInputPassword1').val(); // Igualmente aquí, solamente el campo de password.
+
+        if (!validarEmail(email)) {
+            $('#emailError').text('Introduzca un corre electrónico válido.');
+            return;
+        }
+
+        
+        if (password.length < 8) {
+            $('#passwordError').text('La contraseña debe tener al menos 8 caracteres.'); // Mensaje de excepción, ante un errore de la condición de password, ya que debe cu,plir una longitud menor a 8.
+            return;
+        }
+    });
+
+    
+    function validarEmail(email) { // Función para vlaidar email 
+        var valoresRequeridos = /\S+@\S+\.\S+/; // Verificar valores antes del "@" y después del "@"
+        return valoresRequeridos.test(email);
+    }
+});
---
+++ JS/verificarRegistro.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+    $('#registerForm').submit(function(event) {
+        event.preventDefault(); // Evitar elementos en blanco
+
+        $('.invalid-feedback').text(''); // Eliminar mensaje sde error
+        // Recibir valores dentro de cada input.
+        var rut = $('#rut').val();
+        var k = $('#k').val();
+        var nombres = $('#nombres').val();
+        var apellidos = $('#apellidos').val();
+        var email = $('#email').val();
+        var contrasena = $('#contrasena').val();
+        var confirmarContrasena = $('#confirmarContrasena').val();
+
+        // Siblings: Busca los elementos que sean "Hermanos", esto es
+        // debido a la jerarquía de los divs dentro del DOM.
+
+        // Validar el RUT
+        if (rut.trim() === '') { // Elimina los espacios en blancos del campo para evitar que esté rellenado con espacios en blanco.
+            $('#rut').addClass('is-invalid'); // Selecciona y verifica el elemento "rut" desde el DOM
+            $('#rut').siblings('.invalid-feedback').text('El RUT es requerido.');
+            return;
+        }
+
+        // Validar el dígito verificador
+        if (k.trim() === '') { 
+            $('#k').addClass('is-invalid');
+            $('#k').siblings('.invalid-feedback').text('El dígito verificador es requerido.');
+            return;
+        }
+
+        // Validar los nombres
+        if (nombres.trim() === '') {
+            $('#nombres').addClass('is-invalid');
+            $('#nombres').siblings('.invalid-feedback').text('Ingrese su nombre.');
+            return;
+        }
+
+        // Validar los apellidos
+        if (apellidos.trim() === '') {
+            $('#apellidos').addClass('is-invalid');
+            $('#apellidos').siblings('.invalid-feedback').text('Ingrese sus apellidos.');
+            return;
+        }
+
+        // Validar el formato de correo electrónico
+        if (!validateEmail(email)) {
+            $('#email').addClass('is-invalid');
+            $('#email').siblings('.invalid-feedback').text('Introduzca un correo electrónico válido');
+            return;
+        }
+
+        // Validar la contraseña
+        if (contrasena.trim() === '') {
+            $('#contrasena').addClass('is-invalid');
+            $('#contrasena').siblings('.invalid-feedback').text('La contraseña es requerida.');
+            return;
+        }
+
+        // Validar la longitud de la contraseña
+        if (contrasena.length < 8) {
+            $('#contrasena').addClass('is-invalid');
+            $('#contrasena').siblings('.invalid-feedback').text('La contraseña debe tener al menos 8 caracteres.');
+            return;
+        }
+
+        // Validar que las contraseñas coincidan
+        if (contrasena !== confirmarContrasena) {
+            $('#confirmarContrasena').addClass('is-invalid');
+            $('#confirmarContrasena').siblings('.invalid-feedback').text('Las contraseñas deben coincidir.');
+            return;
+        }
+    });
+
+    // Función para validar el formato del correo electrónico
+    function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+});
