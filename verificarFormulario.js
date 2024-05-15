aca79625ae7af6a4d0db2de9f23d5a7c096b8da2
bNegro <72948318+BNegro1@users.noreply.github.com>
2024-05-15 12:44:54 -0400

    act

--- verificarFormulario.js
+++ verificarFormulario.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
     $('#loginForm').submit(function(event) {
-        
         event.preventDefault(); // Evitar el envío del formulario por defecto
-
-
         $('.error-message').text(''); // Limpiar mensajes de errores
-
         
         var email = $('#exampleInputEmail1').val(); // Recuperamos email del campo de "email".
         var password = $('#exampleInputPassword1').val(); // Igualmente aquí, solamente el campo de password.
@@ -14,16 +10,14 @@ $(document).ready(function() {
             $('#emailError').text('Introduzca un corre electrónico válido.');
             return;
         }
-
         
         if (password.length < 8) {
-            $('#passwordError').text('La contraseña debe tener al menos 8 caracteres.'); // Mensaje de excepción, ante un errore de la condición de password, ya que debe cu,plir una longitud menor a 8.
+            $('#passwordError').text('La contraseña debe tener al menos 8 caracteres.');
             return;
         }
     });
 
-    
-    function validarEmail(email) { // Función para vlaidar email 
+    function validarEmail(email) { // Función para validar email 
         var valoresRequeridos = /\S+@\S+\.\S+/; // Verificar valores antes del "@" y después del "@"
         return valoresRequeridos.test(email);
     }
