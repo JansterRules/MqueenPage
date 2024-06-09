25a456fb89872b39d90a929d5814efa20957b8d4
bNegro <72948318+BNegro1@users.noreply.github.com>
2024-06-09 19:37:49 -0400

    Modificaciones breves

--- Django/misProyectos/taller_makween/models.py
+++ Django/misProyectos/taller_makween/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
-# Modelo de Vehículo (retiramos los anteriores s por mero hecho de no utilizarlos)
 class Vehiculo(models.Model):
-    placa  = models.CharField(max_length=10, primary_key=True)
-    marca  = models.CharField(max_length=50)
+    placa = models.CharField(max_length=10, primary_key=True)
+    marca = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
-    año    = models.IntegerField()
+    año = models.IntegerField()
 
     def __str__(self):
-        return f"{self.marca} {self.modelo} ({self.placa})"
+        return self.placa
+
+'''
+NO OLVIDAR!!!!:
+
+
+python manage.py makemigrations
+python manage.py migrate
+
+
+
+'''
--- Django/misProyectos/taller_makween/views.py
+++ Django/misProyectos/taller_makween/views.py
@@ -1,41 +1,41 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Vehiculo
 
 def index(request):
-    context={}
-    return render(request, 'taller_makween/index.html')
+    context = {}
+    return render(request, 'taller_makween/index.html', context)
 
 def base(request):
-    context={}
-    return render(request, 'taller_makween/base.html')
+    context = {}
+    return render(request, 'taller_makween/base.html', context)
 
 def aboutus(request):
-    context={}
-    return render(request, 'taller_makween/aboutus.html')
+    context = {}
+    return render(request, 'taller_makween/aboutus.html', context)
 
 def Checkout(request):
-    context={}
-    return render(request, 'taller_makween/Checkout.html')
+    context = {}
+    return render(request, 'taller_makween/Checkout.html', context)
 
 def faq(request):
-    context={}
-    return render(request, 'taller_makween/faq.html')
+    context = {}
+    return render(request, 'taller_makween/faq.html', context)
 
 def galeria(request):
-    context={}
-    return render(request, 'taller_makween/galeria.html')
+    context = {}
+    return render(request, 'taller_makween/galeria.html', context)
 
 def login(request):
-    context={}
-    return render(request, 'taller_makween/login.html')
+    context = {}
+    return render(request, 'taller_makween/login.html', context)
 
 def register(request):
-    context={}
-    return render(request, 'taller_makween/register.html',)
+    context = {}
+    return render(request, 'taller_makween/register.html', context)
 
 def regvacante(request):
-    context={}
-    return render(request, 'taller_makween/regvacante.html',)
+    context = {}
+    return render(request, 'taller_makween/regvacante.html', context)
 
 def crud(request):
     vehiculos = Vehiculo.objects.all()
@@ -49,13 +49,13 @@ def vehiculosAdd(request):
         modelo = request.POST['modelo']
         año = request.POST['año']
         Vehiculo.objects.create(placa=placa, marca=marca, modelo=modelo, año=año)
-        return redirect('crud')
+        return crud(request) # Activar renderizardo
     return render(request, 'taller_makween/vehiculos_add.html')
 
 def vehiculos_del(request, pk):
     vehiculo = Vehiculo.objects.get(placa=pk)
     vehiculo.delete()
-    return redirect('crud')
+    return crud(request)  # Activar renderizardo
 
 def vehiculos_findEdit(request, pk):
     vehiculo = Vehiculo.objects.get(placa=pk)
@@ -73,4 +73,5 @@ def vehiculosUpdate(request):
         vehiculo.modelo = modelo
         vehiculo.año = año
         vehiculo.save()
-        return redirect('crud')
+        return crud(request)  # Renderizar la vista crud
+    return render(request, 'taller_makween/vehiculos_edit.html')
